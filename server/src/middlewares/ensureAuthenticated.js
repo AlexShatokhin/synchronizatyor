809b1f0ea7d0adc0f9e3a1590bf97426f9dfcb18
Alex Shatokhin <shatokhin.04@inbox.ru>
2024-12-11 23:30:54 +0200

    edit middleware: link user_id to req; add new field in logs

--- server/src/middlewares/ensureAuthenticated.js
+++ server/src/middlewares/ensureAuthenticated.js
@@ -1,7 +1,25 @@
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
 // Middleware для проверки авторизации
-module.exports = function ensureAuthenticated(req, res, next) {
+module.exports = async function ensureAuthenticated(req, res, next) {
     if (req.session && req.session.userId) {
-        return next(); // Если пользователь авторизован, переходим к следующему обработчику
+        try {
+            // Найдите пользователя в базе данных по userId
+            const user = await prisma.users.findUnique({
+                where: { id: req.session.userId },
+            });
+
+            if (!user) {
+                return res.status(401).json({ message: 'Unauthorized. User not found.' });
+            }
+
+            // Добавьте информацию о пользователе в req.user
+            req.user = user;
+            return next(); // Если пользователь авторизован, переходим к следующему обработчику
+        } catch (error) {
+            return res.status(500).json({ message: 'Internal server error.' });
+        }
     }
     return res.status(401).json({ message: 'Unauthorized. Please log in first.' });
-}
+};
