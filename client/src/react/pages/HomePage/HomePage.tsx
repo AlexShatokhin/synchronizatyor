288c79bb258d8ece2aeb61191d3e8a6c34b65088
Alex Shatokhin <shatokhin.04@inbox.ru>
2024-12-13 09:10:28 +0200

    add error boundary to home page

--- client/src/react/pages/HomePage/HomePage.tsx
+++ client/src/react/pages/HomePage/HomePage.tsx
@@ -1,12 +1,15 @@
 import { FC } from "react";
 import NavigationMenu from "../../modules/NavigationMenu"
+import ErrorBoundary from "../../components/ErrorBoundary/ErrorBoundary";
 import PageWrapper from "../../UI/PageWrapper/PageWrapper";
 
 const HomePage : FC = () => {
     return (
         <PageWrapper>
             <NavigationMenu />
-            <h1>Homepage</h1>
+            <ErrorBoundary>
+                <h1>Homepage</h1>
+            </ErrorBoundary>
         </PageWrapper>
     )
 }
